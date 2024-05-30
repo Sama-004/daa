f8026d9359545d902356814152dd61131689aeb3
sama-004 <70210929+Sama-004@users.noreply.github.com>
2024-05-30 09:00:53 +0530

    add knapSack

---
+++ knapSack.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+// Function to return the maximum of two integers
+int max(int a, int b)
+{
+    return (a > b) ? a : b;
+}
+
+// Function to solve the knapsack problem
+int knapsack(int W, int wt[], int val[], int n)
+{
+    int i, w;
+    int K[n + 1][W + 1];
+
+    // Build table K[][] in bottom up manner
+    for (i = 0; i <= n; i++)
+    {
+        for (w = 0; w <= W; w++)
+        {
+            if (i == 0 || w == 0)
+                K[i][w] = 0;
+            else if (wt[i - 1] <= w)
+                K[i][w] = max(val[i - 1] + K[i - 1][w - wt[i - 1]], K[i - 1][w]);
+            else
+                K[i][w] = K[i - 1][w];
+        }
+    }
+
+    // Return the maximum value that can be put in a knapsack of capacity W
+    return K[n][W];
+}
+
+int main()
+{
+    int val[] = {60, 100, 120};           // values of items
+    int wt[] = {10, 20, 30};              // weights of items
+    int W = 50;                           // maximum capacity of knapsack
+    int n = sizeof(val) / sizeof(val[0]); // number of items
+
+    printf("Maximum value in Knapsack = %d\n", knapsack(W, wt, val, n));
+    return 0;
+}
